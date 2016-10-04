899d20fc6b8a6c7f359e93fa6997cc695d483dc1
Stephen Jung Huh <stephenhuh@linux2.cs.uchicago.edu>
2016-10-04 15:29:39 -0500

    qualtrics

---
+++ real-interviews/qualtricsprep.js
@@ -0,0 +1,29 @@
+//Check if characters of a given string can form a palindrome
+
+/*
+ *  Palindromes -- 
+ *  racecar - length 7
+ *  wow   - length 3
+ *  aabbaa - length 6
+ *  seeeees - length 7
+ * 
+ * Observation all character counts must be even except one character that is in the middle when length is odd
+ * Dont forget the main point, a string reversed and equal is by definition a palindrome
+ *
+ * Just reverse honestly, you can use an array as the data structure to check for characters if there is more complicated processing going on
+ *
+ */
+
+//Binary Trees in JS
+
+
+
+
+
+
+
+
+
+
+
+
