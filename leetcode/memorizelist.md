b270af360dc316a6e47123bc5c38240e449ce420
Stevie Jay <stephenhuh@gmail.com>
2019-03-29 22:14:23 -0400

    lul qs

---
+++ leetcode/memorizelist.md
@@ -0,0 +1,5 @@
+Problems & templates you need to  memorize.
+- Reverse Linked List
+- LRU Cache
+- Sliding window template
+
---
+++ lis.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+const nums = [10, 11, 12, 9, 1,2,3,4];
+
+
+console.log(lis(1, nums, 1, 1, -1));
