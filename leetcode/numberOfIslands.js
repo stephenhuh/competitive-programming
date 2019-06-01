d8058e7347f60d8b0c6084d79b32d3b18daa1d27
Stevie Jay <stephenhuh@gmail.com>
2019-06-01 10:30:51 -0400

    number Islands BFS

--- leetcode/numberOfIslands.js
+++ leetcode/numberOfIslands.js
@@ -1,13 +1,22 @@
+/**
+ * Uses both DFS or BFS. BFS seems to TLE on OJ
+ * @param {character[][]} grid
+ * @return {number}
+ */
 var numIslands = function(grid) {
     if (grid.length === 0 || grid[0].length === 0) return 0;
-    let numRows = grid.length;
-    let numCols = grid[0].length;
+    const numRows = grid.length;
+    const numCols = grid[0].length;
     let count = 0;
+    const queue = [];
     for (let i = 0; i < numRows; i++) {
         for (let j = 0; j < numCols; j++) {
             if (grid[i][j] === '1') {
+                grid[i][j] === '0';
                 count++;
-                dfs(grid, i, j);
+                //dfs(grid, i, j);
+                queue.push([i,j]);
+                bfs(grid, queue);
             }
         }
     }
@@ -23,4 +32,20 @@ var dfs = function(grid, i, j) {
     if ( i + 1 < numRows && grid[i+1][j] === '1') dfs(grid, i+1, j)
     if (j + 1 < numCols && grid[i][j+1] === '1') dfs(grid, i, j+1)
     if ( i - 1 >= 0 && grid[i-1][j] === '1') dfs(grid, i-1, j);
+}
+
+//TLE approach
+var bfs = function(grid, queue) {
+    if (queue.length === 0) return;
+    let numCols = grid[0].length;
+    let numRows = grid.length;
+    
+    while (queue.length > 0) {
+        const [i,j] = queue.shift();
+        grid[i][j] = '0';
+        if (i-1 >= 0 && grid[i-1][j] === '1') queue.push([i-1,j]);
+        if (j+1 < numCols && grid[i][j+1] === '1') queue.push([i, j+1]);
+        if (i+1 < numRows && grid[i+1][j] === '1') queue.push([i+1, j]);
+        if (j-1 >= 0 && grid[i][j-1] === '1') queue.push([i, j-1]);
+    }
 }
