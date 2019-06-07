abc3cbeddbe9c228e932d79912ed73acabeeb1be
Stevie Jay <stephenhuh@gmail.com>
2019-06-07 12:56:16 -0400

    done

--- leetcode/setMatrixZeroes.js
+++ leetcode/setMatrixZeroes.js
@@ -72,7 +72,9 @@ var walk = function(copy, i, j) {
 
 
 /**
+ * Still O(mn) because worst case all els are zeroes
  * Just a cleaner alternative to v2 using objects
+ * However, if u use synchronized sets for is and js separately, you get O(m+n)
  */
 var setZeroesv3 = function(matrix) {
     let ijs = [];
@@ -107,4 +109,58 @@ var walk = function(copy, i, j) {
     for (let c = 0; c < copy[0].length; c++) {
         copy[i][c] = 0;
     }
+}
+
+function setZeroesv5(matrix) {
+  let firstColHasZero = false;
+  let firstRowHasZero = false;
+
+  // Check if first col has zero
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
+      break;
+    }
+  }
+
+  // Check if first row has zero
+  for (let j = 0; j < matrix[0].length; j++) {
+    if (matrix[0][j] === 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
+
+  // Use first row and col as flags, set matrix[i][0] and matrix[0][j] to 0 if matrix[i][j] is 0
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  // Zero out cells based on flags in first row and col
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  // Zero out first col
+  if (firstColHasZero) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  // Zero out first row
+  if (firstRowHasZero) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[0][j] = 0;
+    }
+  }
 }
